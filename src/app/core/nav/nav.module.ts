1f86fe8fe7731e5fa69d41c1a84902498ebdcaef
emanherawy <eman.herawy@gmail.com>
2019-03-31 19:53:42 +0200

    add routes to menue

--- src/app/core/nav/nav.module.ts
+++ src/app/core/nav/nav.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild([]),
   ],
   declarations: [NavComponent],
   exports: [NavComponent]
