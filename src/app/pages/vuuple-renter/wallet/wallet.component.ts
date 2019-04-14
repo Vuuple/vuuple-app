36002d793964b97d82a611b3e20daba16c9e47e5
emanherawy <eman.herawy@gmail.com>
2019-04-14 17:00:49 +0200

    integrate with blockchain , still need to fix network connection issue

--- src/app/pages/vuuple-renter/wallet/wallet.component.ts
+++ src/app/pages/vuuple-renter/wallet/wallet.component.ts
@@ -1,23 +1,32 @@
+import { AuthService } from './../../../auth/core/auth.service';
+import { TokenService } from './../../../providers/token/token.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.scss']
+  styleUrls: ['./wallet.component.scss'],
+  providers: [TokenService]
 })
 export class WalletComponent implements OnInit {
-  avialableTokens="4";
-  tokensInUse="5";
-  tokensRedeemd="3.3";
-  faitCurrencyInAccount="3.3";
-  constructor() { 
-    this.avialableTokens;
-    this.tokensInUse;
-    this.tokensRedeemd;
-    this.faitCurrencyInAccount;
-  }
+  avialableTokens = '4';
+  tokensInUse = '5';
+  tokensRedeemd = '3.3';
+  faitCurrencyInAccount = '3.3';
+  cuurentUser;
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.cuurentUser = this.authService.getCuurentUser();
+    this.getBlance();
   }
-
+  async getBlance() {
+    this.avialableTokens = await this.tokenService.getBalanceOf(
+      this.cuurentUser.ethAddress
+    );
+    console.log(this.avialableTokens, 'this.avialableTokens');
+  }
 }
