0407d934eb4b26db92597cca40d624786ead378f
emanherawy <eman.herawy@gmail.com>
2019-04-14 17:08:10 +0200

    disable bootstraping network

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var main_1 = require('./src/assets/js/main');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("./src/assets/js/main");
 var win, serve;
 var args = process.argv.slice(1);
-serve = args.some(function(val) {
-  return val === '--serve';
-});
+serve = args.some(function (val) { return val === '--serve'; });
 // const {  } = require('');
 try {
-  main_1.run();
-} catch (e) {
-  // Catch Error
-  throw e;
+    main_1.run();
+}
+catch (e) {
+    // Catch Error
+    // throw e;
 }
 //# sourceMappingURL=main.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -125,13 +125,13 @@ const run = async () => {
   // );
   // await node.start_docker(dockerfile);
   // need to check for containers healthy
-  const child = await node.list_containers();
-  // const sttus = await node.check_status(child[0]);
-  const health = await node.check_health(child[0]);
-  child.pop();
-  console.log(child, 'child');
-  // console.log(sttus, 'status');
-  console.log(health, 'health');
+  // const child = await node.list_containers();
+  // // const sttus = await node.check_status(child[0]);
+  // const health = await node.check_health(child[0]);
+  // child.pop();
+  // console.log(child, 'child');
+  // // console.log(sttus, 'status');
+  // console.log(health, 'health');
 
   await app.whenReady();
 
