367a38442410f2005fb0c665873c4301ed9ff97e
gfadelallah@gmail.com <gfadelallah@gmail.com>
2019-04-17 12:20:09 +0200

    reportIssue

--- src/app/pages/shared/shared.module.ts
+++ src/app/pages/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ReportIssueComponent } from './report-issue/report-issue.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   declarations: [NotFoundComponent, ProfileComponent,ReportIssueComponent],
   exports: [NotFoundComponent]
