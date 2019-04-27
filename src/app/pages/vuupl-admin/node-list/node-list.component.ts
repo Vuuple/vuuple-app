59edb1e1ea92acdd8378244eb6b2e9da8f065a6d
gfadelallah@gmail.com <gfadelallah@gmail.com>
2019-04-27 12:37:18 +0200

    conditional format Status+fix Pagination

--- src/app/pages/vuupl-admin/node-list/node-list.component.ts
+++ src/app/pages/vuupl-admin/node-list/node-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerApiService } from '../../../providers/server-api/server-api.service';
+import { PagerService } from '../../../providers/pagesService/pager.service';
 
 @Component({
   selector: 'app-node-list',
@@ -9,14 +10,26 @@ import { ServerApiService } from '../../../providers/server-api/server-api.servi
 export class NodeListComponent implements OnInit {
   TotalCompanyRenter="15";
   nodes : any = [];
-  constructor(private serverApiService : ServerApiService) {
+  pager: any;
+  pagedItems: any;
+  pages: { totalItems: number; currentPage: number; pageSize: number; totalPages: number; startPage: number; endPage: number; startIndex: number; endIndex: number; pages: number[]; };
+  constructor(private serverApiService : ServerApiService,private pagerService : PagerService) {
     this.TotalCompanyRenter;
     this.serverApiService.getAllUsers().subscribe( (data : {}) => {
       console.log (data);
       this.nodes = data ;
+      this.setPage(1);
     });
    }
   ngOnInit() {
   }
+  setPage(page: number) {
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.nodes.length, page);
+    // get current page of items
+    this.pagedItems = this.nodes.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    this.pages = this.pager.pages;
+
+}
 
 }
