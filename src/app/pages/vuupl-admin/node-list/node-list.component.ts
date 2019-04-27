eac3c412124aab54ab353bd323026c2df135a0a5
emanherawy <eman.herawy@gmail.com>
2019-04-28 01:33:37 +0200

    fix pageer issue

--- src/app/pages/vuupl-admin/node-list/node-list.component.ts
+++ src/app/pages/vuupl-admin/node-list/node-list.component.ts
@@ -8,28 +8,32 @@ import { PagerService } from '../../../providers/pagesService/pager.service';
   styleUrls: ['./node-list.component.scss']
 })
 export class NodeListComponent implements OnInit {
-  TotalCompanyRenter="15";
-  nodes : any = [];
+  TotalCompanyRenter = '15';
+  nodes: any = [];
   pager: any;
   pagedItems: any;
-  pages: { totalItems: number; currentPage: number; pageSize: number; totalPages: number; startPage: number; endPage: number; startIndex: number; endIndex: number; pages: number[]; };
-  constructor(private serverApiService : ServerApiService,private pagerService : PagerService) {
+  pages: any[];
+  // pages: { totalItems: number; currentPage: number; pageSize: number; totalPages: number; startPage: number; endPage: number; startIndex: number; endIndex: number; pages: number[]; };
+  constructor(
+    private serverApiService: ServerApiService,
+    private pagerService: PagerService
+  ) {
     this.TotalCompanyRenter;
-    this.serverApiService.getAllUsers().subscribe( (data : {}) => {
-      console.log (data);
-      this.nodes = data ;
+    this.serverApiService.getAllUsers().subscribe((data: {}) => {
+      console.log(data);
+      this.nodes = data;
       this.setPage(1);
     });
-   }
-  ngOnInit() {
   }
+  ngOnInit() {}
   setPage(page: number) {
     // get pager object from service
     this.pager = this.pagerService.getPager(this.nodes.length, page);
     // get current page of items
-    this.pagedItems = this.nodes.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    this.pagedItems = this.nodes.slice(
+      this.pager.startIndex,
+      this.pager.endIndex + 1
+    );
     this.pages = this.pager.pages;
-
-}
-
+  }
 }
