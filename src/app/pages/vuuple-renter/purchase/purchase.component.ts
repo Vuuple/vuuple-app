013aa5fc74b68e607ceacb37d4aea65eab9fa0b0
emanherawy <eman.herawy@gmail.com>
2019-05-01 13:58:02 +0200

    add buy token fuctionality

--- src/app/pages/vuuple-renter/purchase/purchase.component.ts
+++ src/app/pages/vuuple-renter/purchase/purchase.component.ts
@@ -1,15 +1,38 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../auth/core/auth.service';
+import { IcoService } from '../../../providers/ico/ico.service';
 
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.scss']
+  styleUrls: ['./purchase.component.scss'],
+  providers: [IcoService]
 })
 export class PurchaseComponent implements OnInit {
-
-  constructor() { }
+  myForm: FormGroup;
+  cuurentUser: any;
+  rate = 0;
+  constructor(
+    private icoService: IcoService,
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.cuurentUser = this.authService.currentUser;
+    this.getRate();
+    this.myForm = this.fb.group({
+      amount: [0, [Validators.required]]
+    });
   }
-
+  async buyToken() {
+    await this.icoService.buyTokens(
+      this.cuurentUser.ethAddress,
+      this.myForm.value.amount
+    );
+  }
+  async getRate() {
+    this.rate = await this.icoService.getRate();
+  }
 }
